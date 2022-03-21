c4fa6148a2df40ba9e592935a1f14a5f1d49c5c4
Avinash Dengani <avinashdengani123@gmail.com>
2022-03-21 16:38:34 +0530

    Check for validations and appeded data in data.xml file on click event of submit button

--- MyActionEvents.java
+++ MyActionEvents.java
@@ -2,5 +2,22 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class MyActionEvents implements ActionListener{
-    public void actionPerformed(ActionEvent ae) {}
+    public void actionPerformed(ActionEvent ae) {
+		
+		//Submit button event
+		if(ae.getActionCommand() == StudentDataForm.submitBtn.getActionCommand()){
+			if(new Validator().checkForValidations(ae)) {
+
+				MyFileWriter dataAppender = new MyFileWriter();
+				dataAppender.appendData(dataAppender.convertDataToXml(StudentDataForm.nameTextField.getText(), StudentDataForm.rollNoTextField.getText(), StudentDataForm.emailTextField.getText(), StudentDataForm.phoneNoTextField.getText()));
+
+				new MyFileReader().setData(new MyFileReader().getData());
+				StudentDataForm.jTable = new Table(StudentDataForm.tableData, StudentDataForm.colheads);    			
+				StudentDataForm.tableScrollPane.setViewportView(StudentDataForm.jTable);
+
+				//Clearing Inputs
+				StudentDataForm.clearInputs();
+			}
+		}
+	}
 }
