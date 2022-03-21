5f609a303bc4dac050a19b77c7c304a46f033247
Avinash Dengani <avinashdengani123@gmail.com>
2022-03-21 16:36:32 +0530

    Add method to set data for table which use regex Mathcer and Pattern

--- MyFileReader.java
+++ MyFileReader.java
@@ -3,6 +3,8 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MyFileReader {
     
@@ -19,11 +21,63 @@ public class MyFileReader {
             try {
                 dataXMLFile.createNewFile();
             } catch(IOException io) {
-                System.out.println("IOException caught: " +io.getMessage());		
+                System.out.println("IOException caught: " + io.getMessage());		
             }
 		}catch(IOException io){
-			System.out.println("IOException caught: " +io.getMessage());
+			System.out.println("IOException caught: " + io.getMessage());
 		}
 		return completeData;
+	}
+
+	void setData(StringBuffer data) {
+		
+		if(!data.equals("") || data.length() != 0 ) {
+			
+			int totalStudents = 0;
+			
+			String totalNumberOfStudentsRegex = "(<Student>)(.*?)(</Student>)";
+			Pattern totalNumberOfStudentsPattern = Pattern.compile(totalNumberOfStudentsRegex);
+			Matcher totalNumberOfStudentsMatcher = totalNumberOfStudentsPattern.matcher(data);
+			
+			while(totalNumberOfStudentsMatcher.find()) {
+				totalStudents++;
+			}
+
+			String studentPattern = "(<Student>)(<RollNo>)(.*?)(</RollNo>)(<Name>)(.*?)(</Name>)(<Phone>)(.*?)(</Phone>)(<Email>)(.*?)(</Email>)(</Student>)";
+			Pattern pattern = Pattern.compile(studentPattern);
+			Matcher matcher = pattern.matcher(data);
+			
+			StudentDataForm.tableData = new String[totalStudents][4];
+			
+            System.out.println("********STUDENT DETAILS************");
+			
+			int i = 0;
+			
+			while(matcher.find()) {
+
+				String studentRollNo = matcher.group(3); 
+				String studentName = matcher.group(6);
+				String studentPhoneNo = matcher.group(9);
+				String studentEmail = matcher.group(12);
+
+				System.out.println();
+				System.out.println("Name: " + studentName);
+				System.out.println("Roll No.: " + studentRollNo);
+				System.out.println("Phone No.: " + studentPhoneNo);
+				System.out.println("Email: " + studentEmail);
+				System.out.println();
+				
+				try {
+					StudentDataForm.tableData[i][0] = studentRollNo;
+					StudentDataForm.tableData[i][1] = studentName;
+					StudentDataForm.tableData[i][2] = studentPhoneNo;
+					StudentDataForm.tableData[i][3] = studentEmail;
+
+				} catch(ArrayIndexOutOfBoundsException aiobe) {
+					aiobe.printStackTrace();
+				}
+				i++;
+			}
+		}
 	}
 }
