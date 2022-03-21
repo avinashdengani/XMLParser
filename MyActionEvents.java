3ca13dd06c8a1e16ba7cbbae35f3eae86054689c
Avinash Dengani <avinashdengani123@gmail.com>
2022-03-21 14:27:49 +0530

    Create a class MyActionEvents which manages ActionEvents

---
+++ MyActionEvents.java
@@ -0,0 +1,6 @@
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MyActionEvents implements ActionListener{
+    public void actionPerformed(ActionEvent ae) {}
+}
