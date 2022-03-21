a3e6794382efac6e86f4b08ec23435332b1ae2dc
Avinash Dengani <avinashdengani123@gmail.com>
2022-03-21 14:26:45 +0530

    Create a class MyKeyEvents which manages all KeyEvents

---
+++ MyKeyEvents.java
@@ -0,0 +1,11 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
+
+public class MyKeyEvents extends KeyAdapter {
+    public void keyTyped(KeyEvent ke) {}
+    
+    public void keyReleased(KeyEvent ke) {}
+  
+    public void keyPressed(KeyEvent ke) {}
+
+}
