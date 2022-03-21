aef107411df79ff03d1cc5444cc8b95f3812ad27
Avinash Dengani <avinashdengani123@gmail.com>
2022-03-21 16:39:00 +0530

    Check for validation after every key realeased

--- MyKeyEvents.java
+++ MyKeyEvents.java
@@ -2,10 +2,8 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyAdapter;
 
 public class MyKeyEvents extends KeyAdapter {
-    public void keyTyped(KeyEvent ke) {}
     
-    public void keyReleased(KeyEvent ke) {}
-  
-    public void keyPressed(KeyEvent ke) {}
-
+    public void keyReleased(KeyEvent ke) {
+        new Validator().checkForValidations(ke);
+    }
 }
